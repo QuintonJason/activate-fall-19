d49ebd889310e340d58fe98ec4108874835c8754
QuintonJason <quinton.jason@gmail.com>
2019-10-10 00:28:00 -0500

    adding cards

---
+++ src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Cards = () => (
+  <div>
+  	<h2 className="cards__headline">Lineup</h2>
+    <div className="cards">
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+      <div className="card">
+        <h3 className="card__title">Talk Topic Blah Blah</h3>
+        <p className="card__speaker">Lynsey Gwin</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Cards;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,23 +4,31 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Contact from "../components/Contact"
+import Cards from "../components/Cards"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <header className="">
+      <h2><span>activate</span> presents</h2>
+      <h1>Lightening Night</h1>
+      <time>December 10 @ 6:30 PM</time>
+    </header>
+
     <div className="landing-section" id="section-1">
       <a href="#0" className="btn">RSVP</a>
       <p className="panton-regular"> Panton W01 Regular </p>
       <p className="panton-bold"> Panton W01 Bold </p>
       <p className="canoodle"> Canoodle W05 Regular </p>
     </div>
-    <div className="landing-section" id="section-2">
-      Section 2
+
+    <div className="landing-section landing-section--cards" id="section-2">
+      <Cards />
     </div>
+
     <div className="landing-section" id="section-3">
-      Section 3
+      <Contact />
     </div>
-    <Contact />
   </Layout>
 )
 
