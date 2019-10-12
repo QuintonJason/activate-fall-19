e7cbff2202c47763928b7095c571fed84865ad9e
QuintonJason <quinton.jason@gmail.com>
2019-10-12 15:23:57 -0500

    bg color cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,12 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header className="l-header landing-section">
-    <h2 className="header__subhead"><span className="header__subhead--bold">activate</span> presents</h2>
-    <h1 className="header__display">Lightening Night</h1>
-    <time className="header__time"><span className="header__time-bold">December 10</span> @ 6:30 PM</time>
+  <header className="header-wrapper">
+    <div className="l-header landing-section">
+      <h2 className="header__subhead"><span className="header__subhead--bold">activate</span> presents</h2>
+      <h1 className="header__display">Lightening Night</h1>
+      <time className="header__time"><span className="header__time-bold">December 10</span> @ 6:30 PM</time>
+    </div>
   </header>
 )
 
