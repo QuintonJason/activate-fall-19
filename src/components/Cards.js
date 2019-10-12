7b1af3697371d7ad7e49724739cc0dac8652f049
QuintonJason <quinton.jason@gmail.com>
2019-10-12 17:46:09 -0500

    pull data from speaker cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const Cards = () => (
-  <div className="cards-wrapper">
-  	<h2 className="t-section-header cards__headline">Lineup</h2>
-    <div className="cards">
-      <div className="card">
+import speakers from "../../data/speakers.json"
+import SpeakerCard from "./SpeakerCard"
+
+const Cards = props => {
+    const speakerCards = speakers.map(item => <SpeakerCard key={item.id} {...item} />);
+
+    return (
+      <div className="cards-wrapper">
+  	    <h2 className="t-section-header cards__headline">Lineup</h2>
+        <div className="cards">{speakerCards}</div>
+      </div>
+    )
+
+      {/*<div className="card">
         <h3 className="card__title">Talk Topic Blah Blah</h3>
         <p className="card__speaker">Lynsey Gwin</p>
       </div>
@@ -43,9 +52,7 @@ const Cards = () => (
       <div className="card">
         <h3 className="card__title">Talk Topic Blah Blah</h3>
         <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-    </div>
-  </div>
-)
+      </div> */}
+};
 
 export default Cards;
---
+++ src/components/SpeakerCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SpeakerCard = props => {
+  const {id, speakerName, talkTitle} = props;
+
+  return (
+    <div className="card" data-id="{props.id}">
+      <h3 className="card__title">{props.speakerName}</h3>
+      <p className="card__speaker">{props.talkTitle}</p>
+    </div>
+  )
+}
+
+export default SpeakerCard;
