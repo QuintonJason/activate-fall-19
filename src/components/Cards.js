9edbdc781d12d6b2e6a327e2898d532ffa27d039
QuintonJason <quinton.jason@gmail.com>
2019-10-17 20:01:10 -0500

    contact styles

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,55 +4,14 @@ import speakers from "../../data/speakers.json"
 import SpeakerCard from "./SpeakerCard"
 
 const Cards = props => {
-    const speakerCards = speakers.map(item => <SpeakerCard key={item.id} {...item} />);
+  const speakerCards = speakers.map(item => <SpeakerCard key={item.id} {...item} />);
 
-    return (
-      <div className="cards-wrapper">
-  	    <h2 className="t-section-header cards__headline">Lineup</h2>
-        <div className="cards">{speakerCards}</div>
-      </div>
-    )
-
-      {/*<div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-      <div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-      <div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-      <div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-      <div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-      <div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-      <div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-      <div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-      <div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div>
-      <div className="card">
-        <h3 className="card__title">Talk Topic Blah Blah</h3>
-        <p className="card__speaker">Lynsey Gwin</p>
-      </div> */}
+  return (
+    <div className="cards-wrapper">
+      <h2 className="t-section-header cards__headline">Lineup</h2>
+      <div className="cards">{speakerCards}</div>
+    </div>
+  )
 };
 
 export default Cards;
