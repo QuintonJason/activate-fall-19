44d99ed53fce66d18099972e685fd75f742a91db
QuintonJason <quinton.jason@gmail.com>
2019-12-02 17:44:06 -0600

    made link and copy updates

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,7 +6,7 @@ const Details = () => (
       <div>
         <h2 className="details__headline">One night. <br />Ten Talks.</h2>
         <time className="details__time" datetime="2020-01-14T18:30:00">January 14 @ 6:30-8:30 PM</time>
-        <span className="details__location">Location TBD</span>
+        <span className="details__location">LSU Business Education Complex</span>
       </div>
       <div className="details__info">
         <p className="details__description">Lightning Night is a new event for the tech community, to help connect local professionals who want to learn, ask questions, network, or even collaborate. In a series of compact 10-minute talks, you will learn how local leaders are using new technologies, preparing for the future of tech, and getting ahead with their projects. Join us now to reserve your spot!</p>
