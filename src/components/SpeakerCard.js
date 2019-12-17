6dc6e2b734200dba55a23b678ef62140c4ceeaa8
QuintonJason <quinton.jason@gmail.com>
2019-12-16 20:13:29 -0600

    updated cards

--- src/components/SpeakerCard.js
+++ src/components/SpeakerCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const SpeakerCard = props => {
-  const {id, speakerName, talkTitle} = props;
+  const {id, speakerName, talkTitle, company} = props;
 
   return (
     <div className="card" data-id="{props.id}">
       <div className="card__temp-overlay"></div>
-      <h3 className="card__title">{props.speakerName}</h3>
-      <p className="card__speaker">{props.talkTitle}</p>
+      <h3 className="card__title">{props.talkTitle}</h3>
+      <p className="card__speaker">{props.speakerName}, {props.company}</p>
     </div>
   )
 }
