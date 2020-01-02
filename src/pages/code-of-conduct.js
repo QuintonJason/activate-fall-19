8bcd818e99a1f4c69263c8fc4eab8efc9df165d9
QuintonJason <quinton.jason@gmail.com>
2020-01-02 13:21:13 -0600

    code of conduct

--- src/pages/code-of-conduct.js
+++ src/pages/code-of-conduct.js
@@ -4,13 +4,23 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const CodeOfConduct = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Code of Conduct" />
+    <div className="landing-section landing-section--code-of-conduct" id="section-1">
+      <h2>Code of Conduct</h2>
+      <p>All attendees, speakers, staff, sponsors and volunteers are expected to comply with the following code of conduct to ensure a safe and pleasant environment for everyone.</p>
+      <h3>The Details</h3>
+      <p>Activate Pro Incorporated is dedicated to providing a harassment-free experience for everyone attending or participating in the Lightning Night. We will not tolerate harassment of or discrimination against anyone, regardless of gender, race, religion, age, sexual orientation, disability or physical appearance. Harassment can include offensive comments, disruptive behaviour during conference sessions, physical intimidation, stalking, following, photographing or recording an unwilling participant, inappropriate physical contact and unwelcome sexual attention.</p>
+      <p>Sexualised language or imagery is not appropriate in presentations, exhibit stands, workshops or discussions (on or offline) at the conference and will not be permitted.</p>
+      <p>Harassment of event participants online or in electronic venues will be treated as seriously as physical harassment. If you aren’t sure what constitutes harassment, err on the side of caution and restrict your contact.</p>
+      <p>Anyone engaging in harassing behaviour will be asked to stop immediately. We reserve the right to dismiss anyone from the conference without a refund for failure to comply with our anti-harassment policy.</p>
+      <p>If you are being harassed or notice someone else being harassed, please contact a member of the Activate team. We will be happy to assist in contacting the appropriate authorities as necessary and providing support to ensure anyone on the receiving end of these behaviours feels safe for the duration of the event.</p>
+      <h3>NEED HELP? HERE’S HOW TO CONTACT US.</h3>
+      <p>During Lightning Night, you can find a member of Activate team at all times at the registration desk. Please come and talk to us if you need assistance or if there’s anything we can do to help improve your experience of the conference. You can also reach us at any time via email at <a href="mailto:activateproinc@gmail.com">activateproinc@gmail.com</a></p>
+      <p>This work is licensed under a <a href="http://creativecommons.org/licenses/by/3.0/deed.en_US" rel="nofollow noreferrer">Creative Commons Attribution 3.0 Unported License.</a></p>
+    </div>
   </Layout>
 )
 
-export default SecondPage
+export default CodeOfConduct
