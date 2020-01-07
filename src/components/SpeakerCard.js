93e9cacface7e19de998497a2f6b3af88c9b0b23
QuintonJason <quinton.jason@gmail.com>
2020-01-07 11:29:39 -0600

    updated speakers

--- src/components/SpeakerCard.js
+++ src/components/SpeakerCard.js
@@ -5,7 +5,6 @@ const SpeakerCard = props => {
 
   return (
     <div className="card" data-id="{props.id}">
-      <div className="card__temp-overlay"></div>
       <h3 className="card__title">{props.talkTitle}</h3>
       <p className="card__speaker">{props.speakerName}, {props.company}</p>
     </div>
